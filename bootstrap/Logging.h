cf79f4e2eb810cadc1d5c4cfbc5d2326e328ea78
Tughi <one@tughi.com>
2019-12-07 06:42:42 +0100

    Reset first

--- bootstrap/Logging.h
+++ bootstrap/Logging.h
@@ -6,19 +6,19 @@
 
 #define SGR_RESET "\033[0m"
 
-#define SGR_ERROR "\033[37;41m"
-#define SGR_INFO "\033[30;47m"
-#define SGR_WARNING "\033[30;43m"
+#define SGR_ERROR "\033[0;37;41m"
+#define SGR_INFO "\033[0;30;47m"
+#define SGR_WARNING "\033[0;30;43m"
 
-#define SGR_BLACK "\033[30m"
-#define SGR_CYAN "\033[36m"
-#define SGR_DEFAULT "\033[39m"
-#define SGR_GREEN "\033[32m"
-#define SGR_WHITE_BOLD "\033[37;1m"
-#define SGR_YELLOW "\033[33m"
+#define SGR_BLACK "\033[0;30m"
+#define SGR_CYAN "\033[0;36m"
+#define SGR_DEFAULT "\033[0;39m"
+#define SGR_GREEN "\033[0;32m"
+#define SGR_WHITE_BOLD "\033[0;1;37m"
+#define SGR_YELLOW "\033[0;33m"
 
-#define SGR_FAINT_DEFAULT "\033[2;39m"
-#define SGR_FAINT_YELLOW "\033[2;33m"
+#define SGR_FAINT_DEFAULT "\033[0;2;39m"
+#define SGR_FAINT_YELLOW "\033[0;2;33m"
 
 #define LOG(level, source_file, source_line, message) std::cout << level << source_file << ':' << source_line << " -- " << message << SGR_RESET << std::endl
 
