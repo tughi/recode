99dfe6db8f089776efaa8779886c338ba185b015
Tughi <one@tughi.com>
2019-12-14 00:29:14 +0100

    Emit procedure declaration

--- bootstrap/Generator.cpp
+++ bootstrap/Generator.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+std::ostream &operator<<(std::ostream &os, const String *string) {
+    if (string != nullptr) {
+        os << string->data;
+    } else {
+        os << SGR_ERROR << ":String:" << SGR_RESET;
+    }
+    return os;
+}
+
 struct Local {
     String *name;
     int name_suffix;
@@ -18,7 +27,7 @@ struct Context {
 };
 
 String *create_local(Context &context, String *name) {
-    int name_suffix = 0;
+    int name_suffix = -1;
     bool retry;
     do {
         retry = false;
@@ -35,18 +44,20 @@ String *create_local(Context &context, String *name) {
 
     auto unique_name = new String(name->data);
     auto suffix = name_suffix;
-    while (suffix != 0) {
+    while (suffix > 0) {
         unique_name->append(suffix % 10 + '0');
         suffix /= 10;
     }
-    if (name_suffix < 100) {
-        unique_name->append('0');
-    }
-    if (name_suffix < 10) {
-        unique_name->append('0');
-    }
-    if (name_suffix == 0) {
-        unique_name->append('0');
+    if (name_suffix != -1) {
+        if (name_suffix < 100) {
+            unique_name->append('0');
+        }
+        if (name_suffix < 10) {
+            unique_name->append('0');
+        }
+        if (name_suffix == 0) {
+            unique_name->append('0');
+        }
     }
 
     context.first_local = new Local{
@@ -64,21 +75,21 @@ String *create_temp_local(Context &context) {
     return create_local(context, name);
 }
 
-std::ostream &operator<<(std::ostream &os, const String *string) {
-    if (string != nullptr) {
-        os << string->data;
-    } else {
-        os << SGR_ERROR << ":String:" << SGR_RESET;
+String *get_local(Context &context, String *name) {
+    auto local = context.first_local;
+    while (local != nullptr && !local->name->equals(name->data)) {
+        local = local->next;
     }
-    return os;
+    return local->unique_name;
 }
 
-#define EMIT(code) context.file << code << endl
+#define EMIT(code) context.file << code
+#define EMIT_LINE(code) EMIT(code) << endl
 
 String *emit_literal(Context &context, String *destination, Token &token) {
     switch (token.kind) {
     case Token::INTEGER:
-        EMIT("  %" << destination << " = add i32 " << token.integer.value << ", 0");
+        EMIT_LINE("  %" << destination << " = add i32 " << token.integer.value << ", 0");
         break;
     default:
         WARNING(__FILE__, __LINE__, "Unsupported token type: " << token.kind);
@@ -109,11 +120,15 @@ String *emit_expression(Context &context, String *destination, Expression &expre
             ERROR(__FILE__, __LINE__, "Unsupported binary expression operator: " << expression.binary.operator_token->lexeme);
             exit(1);
         }
-        EMIT("  %" << destination << " = " << operation << " i32 %" << left_local << ", %" << right_local);
+        EMIT_LINE("  %" << destination << " = " << operation << " i32 %" << left_local << ", %" << right_local);
         break;
     }
     case Expression::LITERAL: {
         emit_literal(context, destination, *expression.literal.value);
+        break;
+    }
+    case Expression::VARIABLE: {
+        EMIT_LINE("  %" << destination << " = add i32 %" << get_local(context, expression.variable.name->lexeme) << ", 0");
         break;
     }
     default:
@@ -128,15 +143,35 @@ void emit_statement(Context &context, Statement &statement) {
     switch (statement.kind) {
     case Statement::RETURN: {
         auto result = emit_expression(context, create_temp_local(context), *statement.return_expression);
-        EMIT("  ret i32 %" << result);
+        EMIT_LINE("  ret i32 %" << result);
         break;
     }
     case Statement::PROCEDURE_DEFINITION: {
+        auto procedure_name = create_local(context, statement.procedure_definition.name->literal.value->lexeme);
+
+        auto first_outer_local = context.first_local;
+
+        EMIT("define i32 @" << procedure_name << "(");
+        auto parameter = statement.procedure_definition.first_parameter;
+        while (parameter != nullptr) {
+            auto name = create_local(context, parameter->name->lexeme);
+            EMIT("i32 %" << name);
+            parameter = parameter->next;
+            if (parameter != nullptr) {
+                EMIT(", ");
+            }
+        }
+        EMIT_LINE(") {");
+
         auto body_statement = statement.procedure_definition.first_statement;
         while (body_statement != nullptr) {
             emit_statement(context, *body_statement);
             body_statement = body_statement->next;
         }
+
+        EMIT_LINE("}");
+
+        context.first_local = first_outer_local;
         break;
     }
     default:
@@ -149,7 +184,6 @@ void generate(Statement *first_statement) {
     auto context = Context{
         file : ofstream("build/code.ll")
     };
-    EMIT("define i32 @main() {");
 
     Statement *statement = first_statement;
     while (statement != nullptr) {
@@ -157,6 +191,5 @@ void generate(Statement *first_statement) {
         statement = statement->next;
     }
 
-    EMIT("}");
     context.file.close();
 }
