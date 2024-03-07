f66ff78bdaac8a2e8d68238cfd43672413f6ffb2
Tughi <one@tughi.com>
2024-03-07 22:05:44 +0100

    Convert branch and jump instructions

---
+++ tests/ir/007__if_true.s
@@ -0,0 +1,19 @@
+.globl main
+main:
+.L__main__S:
+  sub $0x10, %rsp
+.L__main__1:
+  movl $0x1, 12(%rsp)
+  mov $0x1, %al
+  cmp $0x0, %al
+  jne .L__main__2
+  jmp .L__main__3
+.L__main__2:
+  movl $0x0, 12(%rsp)
+  jmp .L__main__3
+.L__main__3:
+  movl 12(%rsp), %eax
+  jmp .L__main__E
+.L__main__E:
+  add $0x10, %rsp
+  ret
