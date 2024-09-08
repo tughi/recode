f80cab7666b2ebe70e0a010bd8213f6c3459795e
Tughi <one@tughi.com>
2024-09-08 13:37:17 +0200

    Scan Code tokens

---
+++ compiler/Source_Location.h
@@ -0,0 +1,22 @@
+#ifndef __SOURCE_LOCATION_H__
+#define __SOURCE_LOCATION_H__
+
+#include "Source.h"
+#include "String.h"
+#include "Writer.h"
+
+typedef struct Source_Location {
+    Source *source;
+    uint16_t line;
+    uint16_t column;
+} Source_Location;
+
+Source_Location *Source_Location__create(Source *source, uint16_t line, uint16_t column);
+
+void Source_Location__error(Source_Location *self, String *message);
+
+void Source_Location__warning(Source_Location *self, String *message);
+
+Writer *pWriter__write__location(Writer *self, Source_Location *location);
+
+#endif
