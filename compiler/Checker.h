0ec81764b30e71c5499ad1e690b1c393c152f1a8
Tughi <one@tughi.com>
2026-07-06 23:05:15 +0200

    Bootstrap the compiler with a staged `compiler.ir`

--- compiler/Checker.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CHECKER_H__
-#define __CHECKER_H__
-
-#include "Checked_Code.h"
-#include "Parsed_Code.h"
-
-Checked_Source *check(Parsed_Package *parsed_builtin_package, Parsed_Package *parsed_main_package);
-
-#endif
--- compiler/Parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PARSER_H__
-#define __PARSER_H__
-
-#include "Parsed_Code.h"
-
-Parsed_Package *parse_package(String *source_dir, String *package_name, String *binary_name);
-
-#endif
